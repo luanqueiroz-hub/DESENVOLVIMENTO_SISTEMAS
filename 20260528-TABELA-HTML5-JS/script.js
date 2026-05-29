4dcac2ef23f0fe5348182a61aa36027961468dbb
luanqueiroz-hub <queiroz.silva.luan@escola.pr.gov.br>
2026-05-29 00:52:21 +0000

    ok

---
+++ 20260528-TABELA-HTML5-JS/script.js
@@ -0,0 +1,43 @@
+<!-- JAVASCRIPT -->
+<script>
+
+function adicionarAluno(){
+
+// PEGAR OS DADOS DOS INPUTS
+let nome = document.getElementById("nome").value;
+let idade = document.getElementById("idade").value;
+let curso = document.getElementById("curso").value;
+
+// VALIDAR CAMPOS
+if(nome == "" || idade == "" || curso == ""){
+alert("Preencha todos os campos!");
+return;
+}
+
+// PEGAR A TABELA
+let tabela = document.getElementById("tabelaAlunos");
+
+// CRIAR UMA NOVA LINHA
+let novaLinha = tabela.insertRow();
+
+// CRIAR AS CÉLULAS
+let celulaNome = novaLinha.insertCell(0);
+let celulaIdade = novaLinha.insertCell(1);
+let celulaCurso = novaLinha.insertCell(2);
+
+// INSERIR OS DADOS
+celulaNome.innerHTML = nome;
+celulaIdade.innerHTML = idade;
+celulaCurso.innerHTML = curso;
+
+// LIMPAR OS CAMPOS
+document.getElementById("nome").value = "";
+document.getElementById("idade").value = "";
+document.getElementById("curso").value = "";
+
+}
+
+</script>
+
+</body>
+</html>
