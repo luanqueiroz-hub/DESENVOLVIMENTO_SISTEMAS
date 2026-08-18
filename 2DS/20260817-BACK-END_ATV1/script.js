10bc46f734473488f5e75d7631d89c5cc3c79903
luanqueiroz-hub <queiroz.silva.luan@escola.pr.gov.br>
2026-08-18 19:43:33 +0000

    ok

---
+++ 2DS/20260817-BACK-END_ATV1/script.js
@@ -0,0 +1,15 @@
+let nome = "João";
+console.log(nome);
+
+let idade = 25;
+console.log(idade);
+
+let ativo = true;
+console.log(ativo);
+
+let cidade;
+console.log(cidade);
+
+let telefone = null;
+console.log(telefone);
+
