4aeb86420fe7451e51a8863d016fb037e06bd01a
luanqueiroz-hub <queiroz.silva.luan@escola.pr.gov.br>
2026-08-19 16:59:07 +0000

    OK

---
+++ 2DS/20260817-BACK-END_ATV1 (1)/script.js
@@ -0,0 +1,16 @@
+let nome = "João";
+
+let idade = 25;
+
+let ativo = true;
+
+let cidade;
+
+let telefone = null;
+
+let cores = ["amarelo", "branco", "preto"];
+
+let aluno = {
+    nome: "Maria",
+    idade: 22,
+    curso: "Desenvolvimento de Sistemas"};
