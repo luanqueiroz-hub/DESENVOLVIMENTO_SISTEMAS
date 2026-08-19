ec6fc9eceab9c8ba490c815878798d5824aaec5c
luanqueiroz-hub <queiroz.silva.luan@escola.pr.gov.br>
2026-08-19 18:55:13 +0000

    ok

---
+++ 2DS/20260817-BACK-END_ATV1 (4)/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Qual seu nome?");
+console.log(nome);
+
+let idade = prompt("Qual sua idade?");
+console.log(idade);
+
+let profissao = prompt("Qual sua profissão?");
+console.log(profissao);
