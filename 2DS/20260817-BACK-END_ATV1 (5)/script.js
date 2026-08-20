1d464408e20e1150779c7f3f2ecb555d49e17cc6
luanqueiroz-hub <queiroz.silva.luan@escola.pr.gov.br>
2026-08-20 21:27:54 +0000

    ok

---
+++ 2DS/20260817-BACK-END_ATV1 (5)/script.js
@@ -0,0 +1,9 @@
+let nome = prompt("Digite seu nome");
+let idade = prompt("Digite sua idade");
+let cidade = prompt("Digite sua cidade");
+
+let pessoa = {nome, idade, cidade};
+
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa.cidade);
