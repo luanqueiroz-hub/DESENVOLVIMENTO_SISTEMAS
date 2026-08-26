d217d6b77c3f46b9a189b65f3fa74b8a26990688
luanqueiroz-hub <queiroz.silva.luan@escola.pr.gov.br>
2026-08-26 00:08:46 +0000

    ok

---
+++ 2DS/20260818-MOBILE/exercicio6.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // Começa no 6, menor que 1000, e vai pulando de 6 em 6
+        for (int i = 6; i < 1000; i = i + 6) {
+            System.out.println(i);
+        }
+        
+    }
+}
